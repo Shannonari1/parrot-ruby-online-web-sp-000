7d86df35e98c9e3536708b941b07b25d8e157d34
Shannon Young <shannon.ari1@gmail.com>
2020-10-28 19:50:27 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,8 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(sound = "Pretty bird!")
-  puts "Squawk!"
-
+def parrot(squawk)
+  puts squawk
+  return
 end
+
+parrot("pretty bird")
